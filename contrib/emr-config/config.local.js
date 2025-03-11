e06525ac98c1806e8d301e0a94d04b03540d26f2
Dmitry Shutov <dmitry.s@beda.software>
2025-03-11 12:45:22 +0700

    Add FHIr base URL config param

--- contrib/emr-config/config.local.js
+++ contrib/emr-config/config.local.js
@@ -6,6 +6,7 @@ const config = {
 
     tier: 'develop',
     baseURL: 'http://localhost:8080',
+    fhirBaseURL: 'http://localhost:8080/fhir',
     sdcIdeUrl: 'http://localhost:3001',
     aiQuestionnaireBuilderUrl: 'http://localhost:3002',
     sdcBackendUrl: null,
